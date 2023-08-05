ba2f1594b47d6a477707c08b35b54b2ec6743734
Neo Tech Monk <your_email@gmail.com>
2023-08-06 02:19:19 +1000

    abstract Salesforce login

---
+++ src/salesforce.py
@@ -0,0 +1,77 @@
+from dataclasses import asdict, dataclass, field
+
+import yaml
+from simple_salesforce import Salesforce
+from simple_salesforce.exceptions import SalesforceAuthenticationFailed
+
+CONFIG_FILE :str = './config/login.yaml'
+
+# Custom errors
+class ConfigFileNotFoundError(Exception):
+    """Raised when the configuration file is not found."""
+    pass
+
+class ConfigParseError(Exception):
+    """Raised when there's an error parsing the configuration file."""
+    pass
+
+class ConfigValueError(Exception):
+    """Raised when there's an error parsing the configuration file."""
+    pass
+
+class AuthententicationFailedError(Exception):
+    """Raised when SalesforceAuthenticationFailed is raised of Salesforce.session_id is falsely."""
+    pass
+
+@dataclass
+class SalesforceConfig: 
+    username:str
+    password:str
+    security_token:str
+    consumer_key:str
+    consumer_secret :str
+    is_prod: bool = field(default=False, metadata={"exclude": True})
+
+    @property
+    def domain(self) -> str:
+        return 'login' if self.is_prod else 'test'
+    
+    def __post_init__(self):
+        for attr, value in self.__dict__.items():
+            if value is None:
+                raise ConfigValueError(f"credential attribute '{attr}' should not be empty!")
+
+# Load Salesforce credentials from login.yaml
+
+def read_config(file: str = CONFIG_FILE) -> SalesforceConfig:
+    try:
+        with open(file, 'r') as f:
+            credentials = yaml.safe_load(f)
+        return SalesforceConfig(**credentials)
+    except FileNotFoundError:
+        raise ConfigFileNotFoundError(f"The configuration file {file} was not found.")
+    except yaml.YAMLError as e:
+        raise ConfigParseError(f"Error parsing the YAML file: {str(e)}")
+
+def login(credentials: SalesforceConfig)->Salesforce:
+    try:
+        sf =   Salesforce(
+            username=credentials.username,
+            password=credentials.password,
+            security_token=credentials.security_token,
+            consumer_key=credentials.consumer_key,
+            consumer_secret=credentials.consumer_secret,
+            domain=credentials.domain)
+   
+        if not sf.session_id: 
+            raise AuthententicationFailedError ( "Authentication failed: No session id returned")
+        
+        return sf
+    except SalesforceAuthenticationFailed as e:
+        raise AuthententicationFailedError(f"Authentication failed: {str(e)}")
+
+
+
+if __name__ == "__main__":
+    print(login(read_config()))
+
