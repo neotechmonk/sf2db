1d2f65747b02ab1145d171634b8ae5d92b43d852
Neo Tech Monk <your_email@gmail.com>
2023-08-09 08:16:54 +1000

    bug fix order of optional attribute in DBColumnDefinition

--- src/cmcs/db/table_definitions.py
+++ src/cmcs/db/table_definitions.py
@@ -1,4 +1,5 @@
 import datetime
+from dataclasses import field
 from enum import Enum
 from pprint import pprint
 from typing import List
@@ -14,21 +15,66 @@ from .models import to_dict
 
 
 class SQLAlchemyColumnType(Enum):
+    """Mapping between db_tables.json's column types to SQLAlchemy Column types
+        E.g. 
+        ......
+        {
+                "name": "email",
+                "type": "String",
+                "length": 100
+            },
+        {
+            "name": "created_at",
+            "type": "DateTime"
+        }
+            ....
+    """
     Integer = Integer
     String = String
     Float = Float
     Boolean = Boolean
     DateTime = DateTime
     
-@dataclass
-class DBColumnDefinition():
-    column_name : str
-    column_type:str
-    column_length : int
-    is_primary_key : SQLAlchemyColumnType
 
 @dataclass
-class DBTableDefinition():
+class DBColumnDefinition():
+    """Representation of db_tables.json's column definition
+        E.g.
+            .....
+            {
+                    "name": "id",
+                    "type": "Integer",
+                    "primary_key": true
+                },
+            {
+                "name": "name",
+                "type": "String",
+                "length": 50
+            },
+            ....
+    """
+    column_name : str
+    column_type:str 
+    is_primary_key : SQLAlchemyColumnType
+    column_length: int = field(default=None)  # Length is only applicable to sqlalchemy.String
+
+
+@dataclass
+class DBTableDefinition():  
+    """Representation of db_tables.json's table definition
+    Columns of the tables of type `DBColumnDefinition`
+        E.g.
+            .....
+            {
+            "tablename": "users",
+            "columns": [
+                {
+                    "name": "id",
+                    "type": "Integer",
+                    "primary_key": true
+                },
+            ....
+    """
     table_name : str
     columns: list[DBColumnDefinition]
     
@@ -91,8 +137,9 @@ def create_db_table(table_definition:DBTableDefinition)-> DBTable:
             class_attrs[column.column_name] = Column(column.column_type.value, 
                                                      primary_key=column.is_primary_key)
 
-    dt_table = type('DynamicClass', (DBTable,), class_attrs)
+    dt_table = type(table_definition.table_name, (DBTable,), class_attrs)
     return dt_table
+
 
 
 if __name__ == '__main__':
@@ -106,6 +153,8 @@ if __name__ == '__main__':
         "is_active": True}
     for definition in table_definitions: 
         db_table = create_db_table(definition)
+        print(type(db_table))
+
         # user = db_table(id = 232,first_name="John", last_name="Doe", email_address="doe@example.com")
         user = db_table(**user_data)
-        print(to_dict(user))
+        pprint(to_dict(user))
