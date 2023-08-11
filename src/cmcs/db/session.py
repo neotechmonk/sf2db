c4a8a56e545492abc486aabf8860d5ce272cf85e
Neo Tech Monk <your_email@gmail.com>
2023-08-11 23:21:32 +1000

    refactored for readability

--- src/cmcs/db/session.py
+++ src/cmcs/db/session.py
@@ -1,8 +1,5 @@
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from cmcs.util.config import ConfigFiles
 
 from .models import Base
 
@@ -17,8 +14,6 @@ class DBSession:
             'mssql+pyodbc://username:password@server_name/db_name?driver=SQL+Server'
         """
         self.db_uri = db_uri
-        self.engine = None
-        self.session = None
 
     def __enter__(self):
         self.engine = create_engine(self.db_uri)
@@ -27,6 +22,7 @@ class DBSession:
         
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
+
         return self.session
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -35,45 +31,6 @@ class DBSession:
                     self.session.commit()
                 else:
                     self.session.rollback()
-            finally:
+            finally: # ?: overkill? already automatic behaviour?
                 self.session.close()
                 self.engine.dispose()
-
-
-###
-### SAMPLE Table creation 
-###
-# Base = declarative_base()
-
-# Define the User class representing the 'users' table
-# class User(Base):
-#     __tablename__ = 'users'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(50))
-#     email = Column(String(100))
-
-# def create_users_table(db_uri):
-#     # Create the engine
-#     engine = create_engine(db_uri)
-
-#     # Create all tables defined in the metadata, including 'users'
-#     Base.metadata.create_all(bind  = engine)
-
-
-    # Usage example with context manager
-# if __name__ == '__main__':
-#     DB_URI = ConfigFiles.DB_URI
-#     create_users_table(DB_URI)
-    
-#     with DBSession(DB_URI) as session:
-        
-#         new_user = User(name='John Doe', email='john@example.com')
-#         session.add(new_user)
-#         # session.commit()
-
-#         user = session.query(User).filter_by(name='John Doe').first()
-#         # session.commit()
-#         print(f"{user.id}, {user.email}, {user.email}")
-
-
-
